bc024baaab5dec44191735e801f75cc21317fe78
Tymoshchenko Mykhailo <mtymo@softserveinc.com>
2020-06-03 12:50:28 +0300

    init

---
+++ CachingPlayer/CachingPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CachingPlayer
+//
+//  Created by Mikhail Timoscenko on 03.06.2020.
+//  Copyright © 2020 SoftServe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
